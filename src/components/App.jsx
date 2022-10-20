1fc0eed0d1939147adde07518af13f08ee8eee49
Anastasiia <guba.anastaysha@gmail.com>
2022-10-20 23:28:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
+import ProfileList from "./Profile/ProfileList";
+import userData from '../data/user';
+
+import StatisticsSection from "./Statistics/StatisticsSection";
+import StatisticsList from "./Statistics/StatisticsList";
+import statisticsData from '../data/data';
+
+import FriendList from "./Friends/FriendList";
+import friendsData from '../data/friends';
+
+import TransactionHistory from "./Transactions/TransactionHistory";
+import TransactionList from "./Transactions/TransactionList";
+import transactionsData from "../data/transactions"
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <section>
+        <ProfileList items={userData} />
+      </section>
+      <section>
+        <StatisticsSection title="Upload stats">
+          <StatisticsList stats={statisticsData} />
+        </StatisticsSection>
+      </section>
+      <section>
+        <FriendList friends={friendsData} />
+      </section>
+      <section>
+        <TransactionHistory>
+          <TransactionList transactions={transactionsData} />
+        </TransactionHistory>
+      </section>
+    </>
   );
 };
